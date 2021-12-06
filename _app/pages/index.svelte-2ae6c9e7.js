311598eb7dec1b70069c1d5fcbd4874ed43609ff
JacobLinCool <jacoblincool@users.noreply.github.com>
2021-12-06 17:27:07 +0000

    deploy: b27d7e621323cd31e62b5ded3f57265aa8b82564

---
+++ _app/pages/index.svelte-2ae6c9e7.js
@@ -0,0 +1,9 @@
+import{S as O,i as P,s as Q,k as H,e as _,t as l,Q as R,d as r,n as I,c as v,a as p,g as n,b as o,f as M,F as e,J as N}from"../chunks/vendor-6f794801.js";function z(m){let f,b,a,i,w,g,k,S,x,t,T,c,C,A,h,J,j,d,q,K;return document.title=f=m[0],{c(){b=H(),a=_("section"),i=_("h1"),w=l("Welcome to "),g=_("b"),k=l(m[0]),S=l("!"),x=H(),t=_("p"),T=l(`This website is created by
+        `),c=_("a"),C=l("JacobLinCool"),A=l(` with
+        `),h=_("a"),J=l("SvelteKit"),j=l(` &
+        `),d=_("a"),q=l("Tailwind"),K=l(`
+        .`),this.h()},l(s){R('[data-svelte="svelte-1uo06u1"]',document.head).forEach(r),b=I(s),a=v(s,"SECTION",{class:!0});var E=p(a);i=v(E,"H1",{});var y=p(i);w=n(y,"Welcome to "),g=v(y,"B",{});var L=p(g);k=n(L,m[0]),L.forEach(r),S=n(y,"!"),y.forEach(r),x=I(E),t=v(E,"P",{});var u=p(t);T=n(u,`This website is created by
+        `),c=v(u,"A",{href:!0,target:!0,class:!0});var W=p(c);C=n(W,"JacobLinCool"),W.forEach(r),A=n(u,` with
+        `),h=v(u,"A",{href:!0,target:!0,class:!0});var B=p(h);J=n(B,"SvelteKit"),B.forEach(r),j=n(u,` &
+        `),d=v(u,"A",{href:!0,target:!0,class:!0});var F=p(d);q=n(F,"Tailwind"),F.forEach(r),K=n(u,`
+        .`),u.forEach(r),E.forEach(r),this.h()},h(){o(c,"href","https://github.com/jacoblincool"),o(c,"target","_blank"),o(c,"class","svelte-7058fm"),o(h,"href","https://kit.svelte.dev"),o(h,"target","_blank"),o(h,"class","svelte-7058fm"),o(d,"href","https://tailwindcss.com/"),o(d,"target","_blank"),o(d,"class","svelte-7058fm"),o(a,"class","svelte-7058fm")},m(s,$){M(s,b,$),M(s,a,$),e(a,i),e(i,w),e(i,g),e(g,k),e(i,S),e(a,x),e(a,t),e(t,T),e(t,c),e(c,C),e(t,A),e(t,h),e(h,J),e(t,j),e(t,d),e(d,q),e(t,K)},p(s,[$]){$&1&&f!==(f=s[0])&&(document.title=f)},i:N,o:N,d(s){s&&r(b),s&&r(a)}}}function D(m,f,b){return["My Solutions",!0]}class U extends O{constructor(f){super();P(this,f,D,z,Q,{title:0,prerender:1})}get title(){return this.$$.ctx[0]}get prerender(){return this.$$.ctx[1]}}export{U as default};
